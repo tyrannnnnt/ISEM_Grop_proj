4b221204ec4ad1c14f7b2ca0b9a25aa7fc4a3e90
tyrannnnnt <1095144689@qq.com>
2023-03-29 01:21:25 +0800

    add toString and test Files

--- others.py
+++ others.py
@@ -1,12 +1,16 @@
 class Goods(object):
     def getValue(self):
-        return self.value
+        return int(self.value)
 
     def getName(self):
-        return self.name
+        return str(self.name)
 
     def getGoodNum(self):
-        return self.goodNum
+        return str(self.goodNum)
+
+    def __str__(self):
+        return "Item Name: " + str(self.name) + "\nItem Value: " + str(self.value) +\
+                "\nGoods Number: " + str(self.goodNum)
 
     def __init__(self, name, value, goodNum):
         self.name = name
@@ -18,6 +22,12 @@ class Customer(object):
     def addMallDollar(self, amount):
         self.mallDollar += amount
 
+    def __str__(self):
+        return "Customer Name: " + str(self.name) +\
+               "\nCustomer Address: " + str(self.address) +\
+               "\nCustomer Number: " + str(self.customerNum) +\
+               "\nMall Dollar: " + str(self.mallDollar)
+
     def __init__(self, name, address, customerNum, mallDollar):
         self.name = name
         self.address = address
@@ -26,5 +36,8 @@ class Customer(object):
 
 
 class Staff(object):
+    def __str__(self):
+        return "Staff Number: " + str(self.staffNum)
+
     def __init__(self, staffNum):
         self.staffNum = staffNum
---
+++ test.py
@@ -0,0 +1,13 @@
+from main import *
+
+# customerList = readCustomer("zzyTest/Customer.xlsx")
+# for i in customerList:
+#     print(str(i))
+
+# staffList = readStaff("zzyTest/Staff.xlsx")
+# for i in staffList:
+#     print(str(i))
+
+staffList = readStaff("zzyTest/Staff.xlsx")
+for i in staffList:
+    print(str(i))
